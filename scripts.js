6001705fa5209921e295bffbf614a5c388258a06
FadeBoyMC - Youtube <tiendatha2006@gmail.com>
2024-02-06 17:13:15 +0700

    New Update

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ async function loadTrainingData() {
   for (const label of labels) {
     const descriptors = [];
     for (let i = 1; i <= 4; i++) {
-      const image = await faceapi.fetchImage(`/data/${label}/${i}.jpeg`);
+      const image = await faceapi.fetchImage(`./data/${label}/${i}.jpeg`);
       const detection = await faceapi
         .detectSingleFace(image)
         .withFaceLandmarks()
@@ -34,9 +34,9 @@ async function loadTrainingData() {
 let faceMatcher;
 async function init() {
   await Promise.all([
-    faceapi.loadSsdMobilenetv1Model("/models"),
-    faceapi.loadFaceRecognitionModel("/models"),
-    faceapi.loadFaceLandmarkModel("/models"),
+    faceapi.loadSsdMobilenetv1Model("./models"),
+    faceapi.loadFaceRecognitionModel("./models"),
+    faceapi.loadFaceLandmarkModel("./models"),
   ]);
 
   Toastify({
